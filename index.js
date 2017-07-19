22933760b52b52f55f4eb1aeeb865dca643eedf8
Jakub Sobieraj <sobiso@gmail.com>
2017-07-19 13:53:49 +0200

    Added "Remember me" functional, options to hide properties in login

--- index.js
+++ index.js
@@ -1,53 +1,26 @@
-'use strict'
+'use strict';
 
-const Hp = require('hemera-plugin')
+const Hp = require('hemera-plugin');
 const _ = require('lodash');
 var jwt = require('jsonwebtoken');
 const Crypto = require('crypto');
+
 // const hemeraJwt = require('hemera-jwt-auth')
 const hemeraMongoo = require('hemera-mongo-store');
 
-function hashPassword(pepper, salt, password, done) {
-
-  hasher(pepper + password + salt, 11111, function (pass) {
-    done(null, {ok: true, pass: pass, salt: salt})
-  })
-}
-
-function hasher (src, rounds, done) {
-  var out = src
-  var i = 0
-
-  // don't chew up the CPU
-  function round () {
-    i++
-    var shasum = Crypto.createHash('sha256')
-    shasum.update(out, 'utf8')
-    out = shasum.digest('hex')
-    if (rounds <= i) {
-      return done(out)
-    }
-    if (0 === i % 88) {
-      return process.nextTick(round)
-    }
-    round()
-  }
-
-  round()
-}
 
 
 exports.plugin = Hp(function hemeraAccount (options, next) {
-  const hemera = this
+  const hemera = this;
+  const UnauthorizedError = hemera.createError("Unauthorized")
+  const errorUnauthorized = new UnauthorizedError("Unauthorized action")
+        errorUnauthorized.statusCode = 401;
+  // get default options from file
   var default_options = require('./default-options.json')
-
-  options = _.extend(options, default_options);
+  // extend default option by plugin option
+  options = _.extend(default_options, options);
   const topic = options.name;
 
-
-  // hemera.use(hemeraMongo, 
-  //   options.db
-  // )
   /**
    * Login to account
    * @param  {JSON}   email, password, rememberme   If rememberme == 1 then expiry time will set to 01-01-2100
@@ -62,26 +35,57 @@ exports.plugin = Hp(function hemeraAccount (options, next) {
     var email = req.email;
     // get password
     var password = req.password;
+    // checking if email and password exists
+    if (!email || !password) {
+      const missingError = new UnauthorizedError("Missing email or password")
+      missingError.statusCode = 401;
+      cb(missingError, null);
+    }
     // hash password
     hashPassword(options.pepper, options.salt, password, (err, result) => {
 
       if (result.ok) {
-        var hashedPass = result.pass;
         // find account in collection in MongoDB
         hemera.act({
           topic: 'mongo-store',
           cmd: 'find',
           collection: options.name,
-          query: {email: email, password: hashedPass}
+          query: {email: email, password: result.pass}
         }, function(err, resp) {
-          // we have account
-          // generate tekn with expiry
-          // push response
+
+          // checking if we get any account
+          if (resp.result.length > 0) { 
+            // we have account
+            var expiry = new Date();
+            // check if is rememberme is set
+            if (req.rememberme && req.rememberme == true) {
+              expiry.setFullYear(2099);
+            } else {
+              expiry.setDate(expiry.getDate() + 1);
+            }
+            expiry = Math.round(expiry.getTime() / 1000);
+
+            // generate token with expiry
+            var token = jwt.sign(
+              { 
+                exp: expiry.toString(),
+                account: hide(resp.result[0], options.login.fields)
+              }, options.secret);
+
+            // push response
+            cb(null, {token: token});
+            
+          } else { // not authorized
+            cb(errorUnauthorized, null);
+          }
+
         });
-
-
         
-      } 
+      } else {
+        // bad hash password
+        cb(err, null); 
+      }
+
     })
 
 
@@ -92,6 +96,56 @@ exports.plugin = Hp(function hemeraAccount (options, next) {
 })
 
 exports.options = {}
+
+function hashPassword(pepper, salt, password, done) {
+  hasher(pepper + password + salt, 11111, function (pass) {
+    done(null, {ok: true, pass: pass, salt: salt});
+  });
+}
+
+/**
+ * Hash password with sha256
+ * @param  {String}   src    String to hash
+ * @param  {Number}   rounds Number of round
+ * @param  {Function} done   Callback function
+ */
+function hasher (src, rounds, done) {
+  var out = src;
+  var i = 0;
+
+  // don't chew up the CPU
+  function round () {
+    i++
+    var shasum = Crypto.createHash('sha256')
+    shasum.update(out, 'utf8')
+    out = shasum.digest('hex')
+    if (rounds <= i) {
+      return done(out)
+    }
+    if (0 === i % 88) {
+      return process.nextTick(round);
+    }
+    round()
+  }
+
+  round()
+}
+
+/**
+ * Hide object property 
+ * @param  {object} args      Object with properties
+ * @param  {array} propnames  Properties to hide (ie. [{"name": "password","hide": true}]
+ * @return {object}           Object with hidden properties
+ */
+function hide (args, propnames) {
+  var outargs = _.extend({}, args)
+  for (var pn of propnames) {
+    if (pn.hide) {
+      outargs[pn.name] = '[HIDDEN]'
+    }
+  }
+  return outargs
+}
 
 exports.attributes = {
   pkg: require('./package.json')
