c238ccdf353f942a0ba63c5159da29ccb3dd2af5
Artur Grabowski <majest.pl@gmail.com>
2017-07-23 22:14:01 +0100

    npm doc script, more documentation

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const jwt = require('jsonwebtoken')
 const utils = require('./utils.js')
 const Uuid = require('node-uuid')
 
+/**
+ * Accounts plugin for hemera
+ * @module account
+ */
 exports.plugin = Hp(function hemeraAccount(options, next) {
     const hemera = this
     const UnauthorizedError = hemera.createError('Unauthorized')
@@ -20,19 +24,98 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
     // extend default option by plugin option
     options = _.defaultsDeep(options, defaultOptions)
 
+    /**
+     * Authenticate existing user
+     */
     hemera.add({
         topic: options.role,
         cmd: 'login'
     }, login)
 
+    /**
+     * Register a new user
+     */
     hemera.add({
         topic: options.role,
         cmd: 'register'
     }, register)
 
-    function prepareUser(args, done) {
+    /**
+     * Register a new user
+     * @param {object} args - Arguments
+     * - email:    primary email address, data store should ensure unique
+     * - name:     full name of user
+     * - active:   status of user, active==true means login succeeds
+     * - password: password text
+     * - confirmed:  user already confirmed, default: false
+     * Generated fields:
+     * - created: date and time of registration
+     * - confirmcode: used for confirmation
+     * @param {function} done - Callback function provided by the framework
+     * @return {object} Hemera error
+     * @return {object} The id of the new user.
+     *
+     * Example: `{"id": "6127389AFC981"}`
+     */
+     register = function(args, done) {
+        checkEmail(args, function(err, res) {
+            if (err) return done(err)
+            prepareUser(args, function(err, res) {
+                if (err) return done(err)
+                saveuser(res, done)
+            })
+        })
+    }
 
-        hemera.log.debug("Preparing user")
+    /**
+     * Login to account
+     * @param {object} args - Arguments
+     * - email:    email address
+     * - password: password text
+     * @param {function} done - Callback function provided by the framework
+     * @return {object} Error via callback
+     * @return {object} Token via callback
+     *
+     * Example: `{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"}`
+     */
+    login = function(args, done) {
+        hemera.log.debug('login')
+
+        var email = args.email
+        var password = args.password
+        var username = args.username
+
+        // checking if email and password exists
+        if ((!email || !password) && (!username || !password)) {
+            const missingError = new UnauthorizedError('Missing email or password')
+            missingError.statusCode = 401
+            return done(missingError, null)
+        }
+
+        // resolve the user first by email or username if provided
+        resolveUser(args, function(err, res) {
+            if (err) return done(err, null)
+
+            // add salt so that we can veryfy the password, add retrieved password
+            args.salt = args.salt || res.salt
+            args.compareWith = res.password
+
+            // verify the password
+            verifyPassword(args, function(err, res) {
+                if (err) return done(err, null)
+
+                // generate the token
+                generateToken(args, function(err, res) {
+                    done(err, res)
+                })
+            })
+        })
+
+        hemera.log.debug('email and password ok : ' + email)
+    }
+
+    function prepareUser(args, done) {
+        hemera.log.debug('Preparing user')
         var user = {}
         user.username = args.username || args.email
         user.email = args.email
@@ -52,8 +135,15 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
         return done(null, user)
     }
 
-    function checkEmail(args, done) {
-
+    /**
+     * Check whether user with the given email already exists in the database
+     * @param {object} args - Arguments
+     * - email:    email address
+     * @param {function} done - Callback function provided by the framework
+     * @return {object} Error via callback
+     * @return {object} Email via callback
+     */
+     checkEmail = function(args, done) {
         hemera.log.debug('Registration. Checking if email ' + args.email + ' exists')
         hemera.act({
             topic: options.store,
@@ -79,14 +169,14 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
 
         var password = void 0 === args.password ? args.pass : args.password
         var repeat = args.repeat
-
+        var badRequest = null
 
         if (_.isUndefined(password)) {
             // autopass if option enabled. Generates the password for test purposes
             if (options.autopass) {
                 password = Uuid()
             } else {
-                var badRequest = new BadRequest('Password not provided')
+                badRequest = new BadRequest('Password not provided')
                 badRequest.statusCode = 400
                 badRequest.code = 'no-password'
                 return done(badRequest, null)
@@ -95,7 +185,7 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
 
         if (_.isUndefined(repeat)) {
             if (options.mustrepeat) {
-                var badRequest = new BadRequest('Password repeat not provided')
+                badRequest = new BadRequest('Password repeat not provided')
                 badRequest.statusCode = 400
                 badRequest.code = 'no-password-repeat'
                 return done(badRequest, null)
@@ -105,7 +195,7 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
         }
 
         if (password !== repeat) {
-            var badRequest = new BadRequest('Passwords do not match')
+            badRequest = new BadRequest('Passwords do not match')
             badRequest.statusCode = 400
             badRequest.code = 'wrong-password-repeat'
             return done(badRequest, null)
@@ -114,13 +204,11 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
         return hashPassword(args, done)
     }
 
-    function saveuser(args, done) {
 
+    function saveuser(args, done) {
         preparePassword(args, function(err, res) {
             hemera.log.info('Saving user ' + args.email)
             if (err) return done(err, null)
-
-
 
             hemera.act({
                 topic: options.store,
@@ -129,29 +217,6 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
                 data: res
             }, function(err, user) {
                 return done(err, user)
-            })
-        })
-    }
-
-    // Register a new user
-    // - nick:     username, data store should ensure unique, alias: username, email used if not present
-    // - email:    primary email address, data store should ensure unique
-    // - name:     full name of user
-    // - active:   status of user, active==true means login succeeds
-    // - password: password text, alias: pass
-    // - confirmed:  user already confirmed, default: false
-    // Generated fields:
-    // - when: date and time of registration
-    // - confirmcode: used for confirmation
-    // Provides:
-    // - success: {ok:true,user:}
-    // - failure: {ok:false,why:,nick:}
-    function register(args, done) {
-        checkEmail(args, function(err, res) {
-            if (err) return done(err)
-            prepareUser(args, function(err, res) {
-                if (err) return done(err)
-                saveuser(res, done)
             })
         })
     }
@@ -181,7 +246,7 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
             }
         }
 
-        hemera.log.debug("Resolving user")
+        hemera.log.debug('Resolving user')
 
         hemera.act({
             topic: options.store,
@@ -191,7 +256,7 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
         }, function(err, resp) {
             if (err) return done(err, null)
 
-            if (resp.result.length == 0) {
+            if (resp.result.length === 0) {
                 const userNotFound = new UnauthorizedError('UserNotFound')
                 userNotFound.statusCode = 401
                 return done(userNotFound, null)
@@ -222,46 +287,8 @@ exports.plugin = Hp(function hemeraAccount(options, next) {
             token: token
         })
     }
-    /**
-     * Login to account
-     * @param  {JSON}   email, password, rememberme   If rememberme == 1 then expiry time will set to 01-01-2100
-     * @return {JSON}   token                         Return token with expiry time hashed
-     */
-    function login(args, done) {
-        hemera.log.debug('login')
 
-        var email = args.email
-        var password = args.password
-        var username = args.username
 
-        // checking if email and password exists
-        if ((!email || !password) && (!username || !password)) {
-            const missingError = new UnauthorizedError('Missing email or password')
-            missingError.statusCode = 401
-            return done(missingError, null)
-        }
-
-        // resolve the user first by email or username if provided
-        resolveUser(args, function(err, res) {
-            if (err) return done(err, null)
-
-            // add salt so that we can veryfy the password, add retrieved password
-            args.salt = args.salt || res.salt
-            args.compareWith = res.password
-
-            // verify the password
-            verifyPassword(args, function(err, res) {
-                if (err) return done(err, null)
-
-                // generate the token
-                generateToken(args, function(err, res) {
-                    done(null, res)
-                })
-            })
-        })
-
-        hemera.log.debug('email and password ok : ' + email)
-    }
 
     function hashPassword(args, done) {
         // 128 bits of salt
